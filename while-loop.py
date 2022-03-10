b450452c9455c3fd9670928f8621dd6ce00eee14
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-09 23:41:39 -0600

    Add files via upload

---
+++ while-loop.py
@@ -0,0 +1,36 @@
+# # while
+# i = 0
+# while 0 < 50:
+#     print(i)
+#     break
+
+# while
+i = 0
+while i < 50:
+    print(i)
+    i = i + 1
+
+# shorthand while
+i = 0 
+while i < 50:
+    print(i)
+    i += 1
+
+# while
+i = 0 
+while i < 50:
+    print(i)
+    i += 1
+else:
+    print('done with all the work')
+
+# while
+i = 0 
+while i < 50:
+    print(i)
+    i += 1
+    break
+else:
+    print('done with all the work')
+
+
